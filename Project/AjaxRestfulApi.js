2c9d6a7de870b9dfe85966146f6aabcc68a4441f
Ahsan-31 <ahsanazhar31@gmail.com>
2024-03-18 23:58:47 +0500

    api fetch and delete function

--- Project/AjaxRestfulApi.js
+++ Project/AjaxRestfulApi.js
@@ -1,19 +1,39 @@
-$(document).ready(function(){
+function fetchStories(){
     $.ajax({
         type:"GET",
         url:"https://usmanlive.com/wp-json/api/stories",
 
         success: function(response){
-                $('#waiting').hide();
+                var table = $("#table-body");
+                table.empty();
                 let tableEntry="<tr>"
                 response.forEach(element => {
-                    tableEntry=tableEntry+"<td>"+element.id+"</td>";
                     tableEntry=tableEntry+"<td>"+element.title+"</td>";
                     tableEntry=tableEntry+"<td>"+element.content+"</td>";
+                    tableEntry=tableEntry+"<td>"+`<div><button class="btn btn-info btn-edit action-buttons" elementID="${element.id}">Edit</button><button class="btn btn-danger btn-del action-buttons" elementID="${element.id}">Delete</button></div>`+"</td>";
                     tableEntry=tableEntry+"</tr>";
                 });
-                $("#table-body").append(tableEntry);
+                table.append(tableEntry);
 
         }
     })
+}
+
+function deleteStory() {
+    let elementID = $(this).attr("elementID");
+    $.ajax({
+      url: "https://usmanlive.com/wp-json/api/stories/" + elementID,
+      method: "DELETE",
+      success: function () {
+        fetchStories(); // Refresh the list after deleting a story
+      },
+      error: function (error) {
+        console.log("Error deleting story:", error);
+      },
+    });
+  }
+
+$(document).ready(function(){
+    fetchStories();
+    $(document).on("click", ".btn-del", deleteStory);
 })
