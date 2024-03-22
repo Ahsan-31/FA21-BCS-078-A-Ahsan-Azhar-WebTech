240a474257a0fd64d7fa20442f8b6f1b421d2222
Ahsan-31 <ahsanazhar31@gmail.com>
2024-03-22 22:05:41 +0500

    API Edit and Create Functions

--- Project/AjaxRestfulApi.js
+++ Project/AjaxRestfulApi.js
@@ -32,8 +32,68 @@ function deleteStory() {
       },
     });
   }
-
+  function submitStory(event) {
+    event.preventDefault();
+    let storyId = $("#createBtn").attr("elementID");
+    var title = $("#Title").val();
+    var content = $("#Content").val();
+    if (storyId) {
+      $.ajax({
+        url: "https://usmanlive.com/wp-json/api/stories/" + storyId,
+        method: "PUT",
+  
+        data: { title, content },
+        success: function () {
+          fetchStories();
+        },
+        error: function (error) {
+          console.error("Error creating story:", error);
+        },
+      });
+    } else {
+      $.ajax({
+        url: "https://usmanlive.com/wp-json/api/stories",
+        method: "POST",
+        data: { title, content },
+        success: function () {
+          fetchStories();
+        },
+        error: function (error) {
+          console.error("Error creating story:", error);
+        },
+      });
+    }
+  }
+function editStory(event) {
+  event.preventDefault();
+  let storyId = $(this).attr("elementID");
+  $.ajax({
+    url: "https://usmanlive.com/wp-json/api/stories/" + storyId,
+    method: "GET",
+    success: function (element) {
+      console.log(element);
+      $("#clearBtn").show();
+      $("#Title").val(element.title);
+      $("#Content").val(element.content);
+      $("#createBtn").html("Update");
+      $("#createBtn").attr("elementID", element.id);
+    },
+    error: function (error) {
+      console.error("Error deleting story:", error);
+    },
+  });
+}
 $(document).ready(function(){
     fetchStories();
     $(document).on("click", ".btn-del", deleteStory);
+    $(document).on("click", ".btn-edit", editStory);
+    $('#Form').submit(submitStory)
+    $("#clearBtn").on("click", function (e) {
+      e.preventDefault();
+      $("#clearBtn").hide();
+      $("#createBtn").removeAttr("elementID");
+      $("#createBtn").html("Create");
+      $("#Title").val("");
+      $("#Content").val("");
+    });
 })
